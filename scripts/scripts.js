c94b1614c7b07aceebdf6a8d329d40a34b116586
Njegos Jerinic <njegos.jerinic@gmail.com>
2026-02-23 16:22:20 +0100

    html and css basic structure

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,5 +9,5 @@ $(document).ready(function() {
     });
 
     // DEMO
-    $('.map').attr('src', 'https://maps.googleapis.com/maps/api/staticmap?center=32.823943,-117.150259&key=AIzaSyCAJz__098vTeQTMMWL6nARxZhvaK9pcsg&zoom=13&scale=2&size=200x300&maptype=roadmap&format=png&visual_refresh=true&markers=size:small%7Ccolor:0xff0000%7Clabel:1%7C32.823943,-117.150259')
+    $('.map').attr('src', 'https://maps.googleapis.com/maps/api/staticmap?center=32.823943,-117.150259&key=AIzaSyCAJz__098vTeQTMMWL6nARxZhvaK9pcsg&zoom=13&scale=2&size=400x1000&maptype=roadmap&format=png&visual_refresh=true&markers=size:small%7Ccolor:0xff0000%7Clabel:1%7C32.823943,-117.150259')
 });
